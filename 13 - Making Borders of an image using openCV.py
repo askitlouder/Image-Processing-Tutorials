7dacd5baa4da3dd03ab8654b9cbc07849d2a083e
Ask It Loud <73113546+askitlouder@users.noreply.github.com>
2020-11-08 12:59:53 +0530

    Update 13 - Making Borders of an image using openCV.py

--- 13 - Making Borders of an image using openCV.py
+++ 13 - Making Borders of an image using openCV.py
@@ -6,7 +6,7 @@ import cv2
 import numpy as np
 
 
-img1 = cv2.imread("H:\\Data\\lion.jpg")
+img1 = cv2.imread("Data\\lion.jpg")
 img1 = cv2.resize(img1,(1000,600))
 #creating image border 
 brdr = cv2.copyMakeBorder(img1,10,10,5,5,cv2.BORDER_CONSTANT,value = [255,0,125])
