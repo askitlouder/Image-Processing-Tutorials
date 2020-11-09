d392b777ebe1f755a128b8baa8e9bc2937a36eca
Ask It Loud <73113546+askitlouder@users.noreply.github.com>
2020-11-09 18:56:38 +0530

    Update 20 - Adaptive Thresholding using OpenCV.py

--- 20 - Adaptive Thresholding using OpenCV.py
+++ 20 - Adaptive Thresholding using OpenCV.py
@@ -13,7 +13,7 @@
 import cv2 
 import numpy as np
 
-img = cv2.imread('H:\\Data\\page.jpg',0)
+img = cv2.imread('Data\\page.jpg',0)
 img = cv2.resize(img,(400,400))
 _, th1 = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY) #simple 
 th2 = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 11, 2) #adaptive
